79f47bd507dbcaa0c633ddc67aa99b2057922bbf
Kelvin <kelvinyounglee47@gmail.com>
2025-01-09 16:03:42 +0200

    created navBar

---
+++ src/components/navBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const navBar=() =>{
+    return (
+        <nav>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/albums">Albums</a></li>
+            <li><a href="/artists">Artist</a></li>
+        </nav>
+    )
+}
+
+export default navBar;
