d6317efe8ec860f1053fe16ae66b13d24b95c719
Kelvin <kelvinyounglee47@gmail.com>
2025-01-13 08:35:28 +0200

    chages not sure

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "../styles/albums.css";
 import { albumData } from "./albumData.jsx";
 
+
+async function searchAlbum(query) {
+  const response = await axios.get(
+      `https://api.spotify.com/v1/search?q=${query}&type=album`,
+      { headers: { Authorization: `Bearer ${token}` } }
+  );
+}
+
+
 function Albums() {
+  const token="BQA5DVjtqB5nOQourniz4p0YYEjr4t_6YUdAmAqQn97Dk-i6TEraZHh3hXSfbW3PYz2_35S9XGQyi5UYcbOU9ziA9wv1LhlVM3-CC5tmzFuuDSOmhYg";
   return (
     <div className="container">
      <h1 className="heading">
